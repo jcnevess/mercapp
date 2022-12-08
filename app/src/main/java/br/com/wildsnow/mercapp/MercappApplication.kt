411d226d2bb3c5a6c57496a461a820bf65b28a13
Júlio César Neves <juliocnevess@gmail.com>
2022-12-08 17:12:45 -0300

    Checks timber configuration

--- app/src/main/java/br/com/wildsnow/mercapp/MercappApplication.kt
+++ app/src/main/java/br/com/wildsnow/mercapp/MercappApplication.kt
@@ -1,9 +1,16 @@
 package br.com.wildsnow.mercapp
 
 import android.app.Application
+import timber.log.Timber
+
 
 class MercappApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Timber.d("Timber started.")
+        }
     }
 }
