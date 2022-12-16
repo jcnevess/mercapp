f30e752e004dda0d1eb4c2b7eedd06e85febc04e
Júlio César Neves <juliocnevess@gmail.com>
2022-12-16 14:59:45 -0300

    Fixes small bug on the keyboard

--- app/src/main/java/br/com/wildsnow/mercapp/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/wildsnow/mercapp/ui/home/HomeFragment.kt
@@ -2,22 +2,23 @@ package br.com.wildsnow.mercapp.ui.home
 
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import br.com.wildsnow.mercapp.R
 import br.com.wildsnow.mercapp.databinding.FragmentHomeBinding
-import timber.log.Timber
 
+
+private const val QUANTITY_PICKER_MIN_VALUE = 1
+private const val QUANTITY_PICKER_MAX_VALUE = Int.MAX_VALUE
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
-
     private lateinit var binding: FragmentHomeBinding
 
     private val homeViewModel = HomeViewModel()
@@ -34,10 +35,9 @@ class HomeFragment : Fragment() {
         binding.quantityPicker.minValue = 0
 
         binding.quantityPicker.apply {
-            minValue = 0
-            maxValue = Int.MAX_VALUE
+            minValue = QUANTITY_PICKER_MIN_VALUE
+            maxValue = QUANTITY_PICKER_MAX_VALUE
             wrapSelectorWheel = false
-            setOnValueChangedListener { picker, oldValue, newValue ->  Timber.d("Value changed")}
         }
 
         val adapter = HomeListAdapter()
@@ -53,12 +53,19 @@ class HomeFragment : Fragment() {
             if(!isAddingItem) {
                 binding.apply {
                     quantityPicker.visibility = View.INVISIBLE
-                    quantityPicker.value = 0
+                    quantityPicker.value = QUANTITY_PICKER_MIN_VALUE
                     productNameEdit.visibility = View.INVISIBLE
                     productNameEdit.text = Editable.Factory().newEditable("")
                     addItemButton.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_add_24, activity?.theme))
                     addItemButton.setOnClickListener { homeViewModel!!.startAddingItem() }
+
+                    // Hide keyboard
+                    view?.let {
+                        val imm = getSystemService(requireContext(), InputMethodManager::class.java)
+                        imm?.hideSoftInputFromWindow(it.windowToken, 0)
+                    }
                 }
+
             } else {
                 binding.apply {
                     quantityPicker.visibility = View.VISIBLE
